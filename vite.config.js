8de31480bf1543956dc93bc2f497486d2492743c
Shabana0406 <135378209+Shabana0406@users.noreply.github.com>
2023-10-14 20:56:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: 'http://localhost:8000/cities', // replace with the IP address of the Homestead machine
+    https: false,
+    cors: false,
+    hmr: {
+        host: 'http://localhost:8000/cities', // replace with the IP address of the Homestead machine
+    }
+},
+
   base: "/Worldwise/",
   plugins: [react(), eslint()],
 });
